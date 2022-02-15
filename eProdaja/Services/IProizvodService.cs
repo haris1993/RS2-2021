2981a8b1fb20b3be880c6003215ad808d818a8ab
Haris Agic <Haris.Agic@applicita.com>
2022-02-15 16:53:19 +0100

    V1 -Git repo, basic RESTAPI, Dependency injection

---
+++ eProdaja/Services/IProizvodService.cs
@@ -0,0 +1,16 @@
+﻿using eProdaja.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public interface IProizvodService
+    {
+        IEnumerable<Proizvod> Get();
+        Proizvod GetById(int id);
+        Proizvod Insert(Proizvod proizvod);
+        Proizvod Update(int id, Proizvod proizvod);
+    }
+}
---
+++ eProdaja/Startup.cs
@@ -0,0 +1,71 @@
+using eProdaja.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen();
+
+            services.AddScoped<IProizvodService, ProizvodService>();
+
+            //transient - svaki resolve kroz konstruktor, dobija novu instancu
+            //scoped - dok je "http request" "ziv" 
+            //singletone - za sve requeste ista instanca
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
